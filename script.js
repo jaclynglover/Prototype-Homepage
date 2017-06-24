6aa412fab1c59b0132ca1ab1f33664bc4aaad546
jg1565 <jg1565@georgetown.edu>
2017-06-23 22:52:20 -0400

    Fixed jQuery code, tweaked HTML and CSS

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-$(document).ready(function)changeBackground () {
-  $('#photoTwo').hide();
-  $('#photoThree').hide();
-  $('#photoFour').hide();
-  var counter = 0;
+// code modeled after Greg Linch's example in class (https://github.com/greglinch/webdevclass-gallery-assignment-2017), and modified to fit my HTML and add the fadeIn() function
+$(document).ready(function() {
+  var slides = $(".photo");
+  var currentPhoto = 0;
 
-function changeOnClick() {
-  $('#next').click(function() {
-  if (counter === 0) {
-    $('#photoOne').show();
-    $('#photoTwo').hide();
-    $('#photoThree').hide();
-    $('#photoFour').hide();
-      counter = counter + 1;
-   };
-  };
-}
+  slides.hide();
+  slides.first().show();
+
+  var slide = slides[currentPhoto];
+    $(".next").click(function() {
+      $(slide).hide();
+      currentPhoto++;
+      if (currentPhoto > 3) {
+        currentPhoto = 0;
+      }
+
+      slide = slides[currentPhoto];
+      $(slide).fadeIn(300);
+      $(slide).show();
+
+    });
+    
+    $(".back").click(function() {
+      $(slide).hide();
+      currentPhoto--;
+      if (currentPhoto < 0) {
+        currentPhoto = 3;
+      }
+
+      slide = slides[currentPhoto];
+      $(slide).fadeIn(300);
+      $(slide).show();
+      
+    });
+});
